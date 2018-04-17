bd450b66fd08bafe90914013dcb26f1885556541
Glenn Ricaud <gri@enonic.com>
2018-04-17 15:27:35 +0200

    ADFS ID Provider - Refactor configuration (#2)

--- src/main/resources/lib/adfs-id-provider/auth/group.js
+++ src/main/resources/lib/adfs-id-provider/auth/group.js
@@ -7,40 +7,40 @@
 // Require libs
 //──────────────────────────────────────────────────────────────────────────────
 var lib = {
-	adfsIdProvider: {
-		array: require('/lib/adfs-id-provider/array'),
-		auth: {
-			sanitizeName: require('/lib/adfs-id-provider/auth/sanitizeName'),
-			user:         require('/lib/adfs-id-provider/auth/user')
-		},
-		context: require('/lib/adfs-id-provider/context'),
-		object:  require('/lib/adfs-id-provider/object')
-	},
-	xp: {
-		auth:   require('/lib/xp/auth'),
-		portal: require('/lib/xp/portal')
-	}
+    adfsIdProvider: {
+        array: require('/lib/adfs-id-provider/array'),
+        auth: {
+            sanitizeName: require('/lib/adfs-id-provider/auth/sanitizeName'),
+            user: require('/lib/adfs-id-provider/auth/user')
+        },
+        context: require('/lib/adfs-id-provider/context'),
+        object: require('/lib/adfs-id-provider/object')
+    },
+    xp: {
+        auth: require('/lib/xp/auth'),
+        portal: require('/lib/xp/portal')
+    }
 };
 
 //──────────────────────────────────────────────────────────────────────────────
 // Alias functions from libs
 //──────────────────────────────────────────────────────────────────────────────
 var inFirstButNotInSecond = lib.adfsIdProvider.array.inFirstButNotInSecond;
-var runAsAdmin            = lib.adfsIdProvider.context.runAsAdmin;
-var isSet                 = lib.adfsIdProvider.object.isSet;
-var toStr                 = lib.adfsIdProvider.object.toStr;
-var valueFromFormat       = lib.adfsIdProvider.object.valueFromFormat;
-var sanitizeName          = lib.adfsIdProvider.auth.sanitizeName.sanitizeName;
-var getGroups             = lib.adfsIdProvider.auth.user.getGroups;
-var addMembers            = lib.xp.auth.addMembers;
-var createGroup           = lib.xp.auth.createGroup;
-var findPrincipals        = lib.xp.auth.findPrincipals;
-var getIdProviderConfig   = lib.xp.auth.getIdProviderConfig;
-var getMembers            = lib.xp.auth.getMembers;
+var runAsAdmin = lib.adfsIdProvider.context.runAsAdmin;
+var isSet = lib.adfsIdProvider.object.isSet;
+var toStr = lib.adfsIdProvider.object.toStr;
+var valueFromFormat = lib.adfsIdProvider.object.valueFromFormat;
+var sanitizeName = lib.adfsIdProvider.auth.sanitizeName.sanitizeName;
+var getGroups = lib.adfsIdProvider.auth.user.getGroups;
+var addMembers = lib.xp.auth.addMembers;
+var createGroup = lib.xp.auth.createGroup;
+var findPrincipals = lib.xp.auth.findPrincipals;
+var getIdProviderConfig = lib.xp.auth.getIdProviderConfig;
+var getMembers = lib.xp.auth.getMembers;
 //var getMemberships        = lib.xp.auth.getMemberships;
-var getPrincipal          = lib.xp.auth.getPrincipal;
-var modifyGroup           = lib.xp.auth.modifyGroup;
-var removeMembers         = lib.xp.auth.removeMembers;
+var getPrincipal = lib.xp.auth.getPrincipal;
+var modifyGroup = lib.xp.auth.modifyGroup;
+var removeMembers = lib.xp.auth.removeMembers;
 
 //──────────────────────────────────────────────────────────────────────────────
 // auth.group methods
@@ -56,34 +56,34 @@ var removeMembers         = lib.xp.auth.removeMembers;
  * @returns {user}
  */
 function createOrModify(params) {
-	log.debug('createOrModify(' + toStr(params) + ')');
+    log.debug('createOrModify(' + toStr(params) + ')');
 
-	var group = runAsAdmin(function() {
-		return getPrincipal('group:' + params.userStore + ':' + params.name);
-	});
-	//log.debug('getPrincipalResult:' + toStr(group));
+    var group = runAsAdmin(function () {
+        return getPrincipal('group:' + params.userStore + ':' + params.name);
+    });
+    //log.debug('getPrincipalResult:' + toStr(group));
 
-	if(group) {
-		if(group.displayName === params.displayName && group.description === params.description) {
-			log.debug('unchanged group:' + toStr(group));
-		} else {
-			group = modifyGroup({
-				key: group._id,
-				editor: function(c) {
-					c.displayName = params.displayName;
-					c.description = params.description;
-					return c;
-				}
-			});
-			log.info('modified group:' + toStr(group));
-		}
-	} else {
-		runAsAdmin(function() {
-			group = createGroup(params);
-		});
-		log.info('created group:' + toStr(group));
-	}
-	return group;
+    if (group) {
+        if (group.displayName === params.displayName && group.description === params.description) {
+            log.debug('unchanged group:' + toStr(group));
+        } else {
+            group = modifyGroup({
+                key: group._id,
+                editor: function (c) {
+                    c.displayName = params.displayName;
+                    c.description = params.description;
+                    return c;
+                }
+            });
+            log.info('modified group:' + toStr(group));
+        }
+    } else {
+        runAsAdmin(function () {
+            group = createGroup(params);
+        });
+        log.info('created group:' + toStr(group));
+    }
+    return group;
 }; // function createOrModify
 exports.createOrModify = createOrModify;
 
@@ -96,14 +96,14 @@ exports.createOrModify = createOrModify;
  * @returns {undefined}
  */
 function addUser(params) {
-	log.debug('addUser(' + toStr(params) + ')');
-	//getMembers(params.groupKey)
-	//getMemberships(params.userKey)
+    log.debug('addUser(' + toStr(params) + ')');
+    //getMembers(params.groupKey)
+    //getMemberships(params.userKey)
 
-	var addMembersResult = runAsAdmin(function() {
-		return addMembers(params.groupKey, [params.userKey]);
-	});
-	log.info('addMembersResult:' + toStr(addMembersResult)); // In Enonic XP 6.9.2 return undefined even if group is unmodified
+    var addMembersResult = runAsAdmin(function () {
+        return addMembers(params.groupKey, [params.userKey]);
+    });
+    log.info('addMembersResult:' + toStr(addMembersResult)); // In Enonic XP 6.9.2 return undefined even if group is unmodified
 };
 exports.addUser = addUser;
 
@@ -116,11 +116,11 @@ exports.addUser = addUser;
  * @returns {undefined}
  */
 function removeUser(params) {
-	log.debug('removeUser(' + toStr(params) + ')');
-	var removeMembersResult = runAsAdmin(function() {
-		return removeMembers(params.groupKey, [params.userKey]);
-	});
-	log.info('removeMembersResult:' + toStr(removeMembersResult));
+    log.debug('removeUser(' + toStr(params) + ')');
+    var removeMembersResult = runAsAdmin(function () {
+        return removeMembers(params.groupKey, [params.userKey]);
+    });
+    log.info('removeMembersResult:' + toStr(removeMembersResult));
 };
 exports.removeUser = removeUser;
 
@@ -132,116 +132,120 @@ exports.removeUser = removeUser;
  * @param {Object} params.user
  * @returns {}
  */
-exports.createAndUpdateGroupsFromJwt = function(params) {
+exports.createAndUpdateGroupsFromJwt = function (params) {
 
-	var idProviderConfig = getIdProviderConfig();
-	log.debug('idProviderConfig:' + toStr(idProviderConfig));
+    var idProviderConfig = getIdProviderConfig();
+    log.debug('idProviderConfig:' + toStr(idProviderConfig));
 
-	var createAndUpdateGroupsOnLogin = isSet(idProviderConfig.createAndUpdateGroupsOnLogin) ? idProviderConfig.createAndUpdateGroupsOnLogin : true;
-	log.debug('createAndUpdateGroupsOnLogin:' + toStr(createAndUpdateGroupsOnLogin));
+    var createAndUpdateGroupsOnLogin = isSet(idProviderConfig.createAndUpdateGroupsOnLogin)
+        ? idProviderConfig.createAndUpdateGroupsOnLogin
+        : true;
+    log.debug('createAndUpdateGroupsOnLogin:' + toStr(createAndUpdateGroupsOnLogin));
 
-	if(!createAndUpdateGroupsOnLogin) { return; }
-	var dnFormat = idProviderConfig.groups	&& idProviderConfig.groups.dn || '${dn}';
-	var dn = valueFromFormat({
-		format: dnFormat,
-		data:   params.jwt.payload
-	});
-	if(!dn) {
-		throw new Error('Could not generate dn from mapping:' + dnFormat);
-	}
-	log.debug('dn:' + toStr(dn));
+    if (!createAndUpdateGroupsOnLogin) {
+        return;
+    }
+    var dnFormat = idProviderConfig.group && idProviderConfig.group.dn || '${dn}';
+    var dn = valueFromFormat({
+        format: dnFormat,
+        data: params.jwt.payload
+    });
+    if (!dn) {
+        throw new Error('Could not generate dn from mapping:' + dnFormat);
+    }
+    log.debug('dn:' + toStr(dn));
 
-	var groupKeysInXp = getGroups(params.user.key).filter(function(group) {
-		return group.description.startsWith('{"dn":'); // Only groups from AD
-	}).map(function(group) {
-		return group.key;
-	});
-	log.debug('groupKeysInXp:' + toStr(groupKeysInXp));
+    var groupKeysInXp = getGroups(params.user.key).filter(function (group) {
+        return group.description.startsWith('{"dn":'); // Only groups from AD
+    }).map(function (group) {
+        return group.key;
+    });
+    log.debug('groupKeysInXp:' + toStr(groupKeysInXp));
 
-	var groupKeysinAd = [];
+    var groupKeysinAd = [];
 
-	var dnArray = [];
-	var pathArray = [];
-	dn.split(',').reverse().forEach(function(rdn) {
-		var rdnParts = rdn.split('=', 2);
-		log.debug('rdnParts:' + toStr(rdnParts));
+    var dnArray = [];
+    var pathArray = [];
+    dn.split(',').reverse().forEach(function (rdn) {
+        var rdnParts = rdn.split('=', 2);
+        log.debug('rdnParts:' + toStr(rdnParts));
 
-		var rdnName = rdnParts[0];
-		log.debug('rdnName:' + toStr(rdnName));
+        var rdnName = rdnParts[0];
+        log.debug('rdnName:' + toStr(rdnName));
 
-		var rdnValue = rdnParts[1]; // TODO: http://www.rlmueller.net/CharactersEscaped.htm
-		log.debug('rdnValue:' + toStr(rdnValue));
+        var rdnValue = rdnParts[1]; // TODO: http://www.rlmueller.net/CharactersEscaped.htm
+        log.debug('rdnValue:' + toStr(rdnValue));
 
-		if(rdnName !== 'CN') {
-			dnArray.push(rdn);
-			if(rdnName === 'OU') { // Not DC
-				pathArray.push(rdnValue);
+        if (rdnName !== 'CN') {
+            dnArray.push(rdn);
+            if (rdnName === 'OU') { // Not DC
+                pathArray.push(rdnValue);
 
-				var path = pathArray.join('/');
-				var createOrModifyParams = {
-					name: sanitizeName(path),
-					displayName: path,
-					description: JSON.stringify({
-						dn: Array.prototype.slice.call(dnArray).reverse().join(',') // NOTE: Operate on a copy since reverse modifies in place.
-					}),
-					userStore: params.user.userStore
-				}
-				log.debug('createOrModifyParams:' + toStr(createOrModifyParams));
+                var path = pathArray.join('/');
+                var createOrModifyParams = {
+                    name: sanitizeName(path),
+                    displayName: path,
+                    description: JSON.stringify({
+                        dn: Array.prototype.slice.call(dnArray).reverse().join(',') // NOTE: Operate on a copy since reverse modifies in place.
+                    }),
+                    userStore: params.user.userStore
+                }
+                log.debug('createOrModifyParams:' + toStr(createOrModifyParams));
 
-				// Process all groups in case the have changes.
-				var group = createOrModify(createOrModifyParams);
+                // Process all groups in case the have changes.
+                var group = createOrModify(createOrModifyParams);
 
-				groupKeysinAd.push(group.key);
-			} // if(rdnName !== 'CN')
-		}
-	});
-	log.debug('groupKeysinAd:' + toStr(groupKeysinAd));
+                groupKeysinAd.push(group.key);
+            } // if(rdnName !== 'CN')
+        }
+    });
+    log.debug('groupKeysinAd:' + toStr(groupKeysinAd));
 
-	var newGroupKeys = inFirstButNotInSecond(groupKeysinAd, groupKeysInXp);
-	log.debug('newGroupKeys:' + toStr(newGroupKeys));
+    var newGroupKeys = inFirstButNotInSecond(groupKeysinAd, groupKeysInXp);
+    log.debug('newGroupKeys:' + toStr(newGroupKeys));
 
-	var oldGroupKeys = inFirstButNotInSecond(groupKeysInXp, groupKeysinAd);
-	log.debug('oldGroupKeys:' + toStr(oldGroupKeys));
+    var oldGroupKeys = inFirstButNotInSecond(groupKeysInXp, groupKeysinAd);
+    log.debug('oldGroupKeys:' + toStr(oldGroupKeys));
 
-	newGroupKeys.forEach(function(groupKey) {
-		addUser({
-			groupKey: groupKey,
-			userKey:  params.user.key
-		});
-	}); // newGroupKeys.forEach
+    newGroupKeys.forEach(function (groupKey) {
+        addUser({
+            groupKey: groupKey,
+            userKey: params.user.key
+        });
+    }); // newGroupKeys.forEach
 
-	oldGroupKeys.forEach(function(groupKey) {
-		removeUser({
-			groupKey: groupKey,
-			userKey:  params.user.key
-		});
-		// Group may become empty, if syncing implemented could check if group still in AD FS.
-	});
+    oldGroupKeys.forEach(function (groupKey) {
+        removeUser({
+            groupKey: groupKey,
+            userKey: params.user.key
+        });
+        // Group may become empty, if syncing implemented could check if group still in AD FS.
+    });
 
 }; // createAndUpdateGroupsFromJwt
 
 
-exports.debugAllGroups = function() {
-	var findPrincipalsResult = runAsAdmin(function() {
-		return findPrincipals({
-			//start: 0,
-			//count: 10,
-			type: 'group',
-			//userStore: 'adfs'
-			//userStore: 'system'
-		});
-	});
-	//log.debug('findPrincipalsResult:' + toStr(findPrincipalsResult));
+exports.debugAllGroups = function () {
+    var findPrincipalsResult = runAsAdmin(function () {
+        return findPrincipals({
+            //start: 0,
+            //count: 10,
+            type: 'group',
+            //userStore: 'adfs'
+            //userStore: 'system'
+        });
+    });
+    //log.debug('findPrincipalsResult:' + toStr(findPrincipalsResult));
 
-	findPrincipalsResult.hits.forEach(function(group) {
-		var getPrincipalResult = runAsAdmin(function() {
-			return getPrincipal(group.key);
-		});
-		//log.debug('getPrincipalResult:' + toStr(getPrincipalResult)); // Does not show members
+    findPrincipalsResult.hits.forEach(function (group) {
+        var getPrincipalResult = runAsAdmin(function () {
+            return getPrincipal(group.key);
+        });
+        //log.debug('getPrincipalResult:' + toStr(getPrincipalResult)); // Does not show members
 
-		var getMembersResult = runAsAdmin(function() {
-			return getMembers(group.key)
-		});
-		log.debug('getMembers('+ group.key +') --> ' + toStr(getMembersResult));
-	});
+        var getMembersResult = runAsAdmin(function () {
+            return getMembers(group.key)
+        });
+        log.debug('getMembers(' + group.key + ') --> ' + toStr(getMembersResult));
+    });
 };
