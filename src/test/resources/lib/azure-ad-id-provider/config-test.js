a45ac89102dd58db9b129b37fda27ac5a9d3352b
Espen Norderud <espen42@gmail.com>
2023-01-04 14:20:03 +0100

    Test config.js

--- src/test/resources/lib/azure-ad-id-provider/config-test.js
+++ src/test/resources/lib/azure-ad-id-provider/config-test.js
@@ -1,17 +1,17 @@
 const test = require('/lib/xp/testing');
 const testUtils = require('/testUtils');
 
-const updateConfigFileMocks = testUtils.mockAndGetUpdaterFunc(
-  "/lib/configFile/configFile.js",
-  {
-    getConfigForIdProvider: () => null // (idProviderName) => mocks.configFile.getConfigForIdProvider(idProviderName)
-  }
-);
-
-const updateAuthMocks = () => testUtils.mockAndGetUpdaterFunc(
+let getIdProviderConfigWasRun;
+test.mock(
   "/lib/xp/auth",
   {
-    getIdProviderConfig: () => null
+    getIdProviderConfig: () => {
+      getIdProviderConfigWasRun = true;
+      return {
+        configOrigin: "data layer (/lib/xp/auth)",
+        wasRun: true
+      }
+    }
   }
 );
 
@@ -20,5 +20,59 @@ test.mock("/lib/xp/portal", {
 });
 
 
+const updateConfigFileMocks = testUtils.mockAndGetUpdaterFunc(
+  "/lib/configFile/configFile.js",
+  {
+    getConfigForIdProvider: () => null // (idProviderName) => mocks.configFile.getConfigForIdProvider(idProviderName)
+  }
+);
+
 
 // Require the lib under test after mocking what it uses:
+const lib = require('./config');
+
+
+exports.test_getIdProviderConfig_fromFile_whenConfigFileReturnsSomething = () => {
+  getIdProviderConfigWasRun = false;
+  updateConfigFileMocks({
+    getConfigForIdProvider: () => ({
+      configStemsFrom: ".cfg file"
+    })
+  });
+
+  const idProviderConfig = lib.getIdProviderConfig();
+
+  test.assertEquals(".cfg file", idProviderConfig.configStemsFrom);
+  test.assertEquals(undefined, idProviderConfig.configOrigin);
+  test.assertFalse(getIdProviderConfigWasRun);
+}
+
+
+
+exports.test_getIdProviderConfig_fromDataLayer_whenConfigFileReturnsNull = () => {
+  getIdProviderConfigWasRun = false;
+  updateConfigFileMocks({
+    getConfigForIdProvider: () => null
+  });
+
+  const idProviderConfig = lib.getIdProviderConfig();
+
+  test.assertEquals("data layer (/lib/xp/auth)", idProviderConfig.configOrigin);
+  test.assertEquals(undefined, idProviderConfig.configStemsFrom);
+  test.assertTrue(getIdProviderConfigWasRun);
+}
+
+
+
+exports.test_getIdProviderConfig_fromDataLayer_whenConfigFileReturnsUndefined = () => {
+  getIdProviderConfigWasRun = false;
+  updateConfigFileMocks({
+    getConfigForIdProvider: () => undefined
+  });
+
+  const idProviderConfig = lib.getIdProviderConfig();
+
+  test.assertEquals("data layer (/lib/xp/auth)", idProviderConfig.configOrigin);
+  test.assertEquals(undefined, idProviderConfig.configStemsFrom);
+  test.assertTrue(getIdProviderConfigWasRun);
+}
