f93d4e0b68f387e17ee28463fa83e6e8dd8d0025
Espen Norderud <espen42@gmail.com>
2023-01-04 16:08:13 +0100

    Fix parsingCallbacks specific for this idprovider

--- src/main/resources/lib/configFile/parsingCallbacks.js
+++ src/main/resources/lib/configFile/parsingCallbacks.js
@@ -1,33 +1,7 @@
-const parseStringArray = (jsonStringValue) => {
-    if (!jsonStringValue) {
-        return [];
-    }
-
-    let parsed;
-    try {
-        parsed = JSON.parse((jsonStringValue || '').trim());
-    } catch (e) {
-        throw Error(`Expected a well-formed JSON string: '${jsonStringValue}'`, e);
-    }
-
-    if (!parsed) {
-        return [];
-    }
-    if (!Array.isArray(parsed)) {
-        throw Error(`Expected a well-formed array: ${JSON.stringify(parsed)}`);
-    }
-
-    return parsed.map((item, i) => {
-        if ('string' !== typeof item) {
-            throw Error(`Item with index ${i} in the array is not a string: ${JSON.stringify(item)}`);
-        }
-        return item.trim();
-    })
+const doubleAtsToDollar = (value) => {
+  return (value || '').replace(/@@\{/g, '${');
 }
-exports.parseStringArray = parseStringArray;
-
-const firstAtsToDollar = (value) => (value || '').replace(/@@\{/, '${');
-exports.firstAtsToDollar= firstAtsToDollar;
+exports.doubleAtsToDollar= doubleAtsToDollar;
 
 
 
@@ -35,10 +9,9 @@ exports.firstAtsToDollar= firstAtsToDollar;
 // For example, "defaultGroups" below is converted to '^idprovider\.[a-zA-Z0-9_-]+\.defaultGroups$' which will match the keys
 // 'idprovider.oidc.defaultGroups' and 'idprovider.other.defaultGroups' in the config file, but not 'idprovider.oidc.tokenUrl'.
 const IDPROVIDER_PARSE_CALLBACKS = {
-    'defaultGroups': parseStringArray,
-    'scopes':  (value) => parseStringArray(value).join(" "),
-    'mappings.displayName': firstAtsToDollar,
-    'mappings.email': firstAtsToDollar
+  'user.name': doubleAtsToDollar,
+  'user.displayName': doubleAtsToDollar,
+  'user.email': doubleAtsToDollar,
 }
 
 
